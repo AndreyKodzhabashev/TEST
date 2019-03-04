92118ceca98d524d66bd1b1a1fcd48eb2e52d0c5
AndreyKo <andrey.kodzhabashev@gmail.com>
2019-03-04 16:12:40 +0200

    CreatedNewProject

---
+++ HowToWorkWithGitHub/01Test_GitHubNewProjectFromPC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01Test_GitHubNewProjectFromPC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
