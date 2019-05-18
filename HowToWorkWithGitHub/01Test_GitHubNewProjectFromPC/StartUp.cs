03ce91d383fae4f3c190597e2a73118f44eb11ec
Andrey Kodzhabashev <37802907+AndreyKodzhabashev@users.noreply.github.com>
2019-05-18 14:24:11 +0300

    Update StartUp.cs

--- HowToWorkWithGitHub/01Test_GitHubNewProjectFromPC/StartUp.cs
+++ HowToWorkWithGitHub/01Test_GitHubNewProjectFromPC/StartUp.cs
@@ -9,9 +9,9 @@ namespace _01Test_GitHubNewProjectFromPC
             Console.WriteLine("First branch updated");
             string secondBranch = "Created secondBranch";
             
-          string test = "Created on GitHub with editing the file"
+          string test = "Created on GitHub with editing the file";
             string test1 = "Created on 18.May.2019 on my PC";
-            string test 3 = "Added again on PC"
+            string test 3 = "Added again on PC";
         }
     }
 }
