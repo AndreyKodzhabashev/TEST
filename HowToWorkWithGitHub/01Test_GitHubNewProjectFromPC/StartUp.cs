58b99df419db9b4968a10ec33c41f9844a659f5f
AndreyKo <andrey.kodzhabashev@gmail.com>
2019-05-18 14:25:30 +0300

    test

--- HowToWorkWithGitHub/01Test_GitHubNewProjectFromPC/StartUp.cs
+++ HowToWorkWithGitHub/01Test_GitHubNewProjectFromPC/StartUp.cs
@@ -8,10 +8,10 @@ namespace _01Test_GitHubNewProjectFromPC
         {
             Console.WriteLine("First branch updated");
             string secondBranch = "Created secondBranch";
-            
-          string test = "Created on GitHub with editing the file"
+
+            string test = "Created on GitHub with editing the file";
             string test1 = "Created on 18.May.2019 on my PC";
-            string test 3 = "Added again on PC"
+            string test3 = "Added again on PC";
         }
     }
 }
