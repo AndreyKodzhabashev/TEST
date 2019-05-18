10a59d4b9c41ba71b5a2d512c462a869a189d3b6
AndreyKo <andrey.kodzhabashev@gmail.com>
2019-05-18 14:14:24 +0300

    Revert "Update StartUp.cs from cloned copy ot my PC"
    This reverts commit 20835d87600ffb033741f3b4efb73f052d500703.

--- HowToWorkWithGitHub/01Test_GitHubNewProjectFromPC/StartUp.cs
+++ HowToWorkWithGitHub/01Test_GitHubNewProjectFromPC/StartUp.cs
@@ -8,7 +8,6 @@ namespace _01Test_GitHubNewProjectFromPC
         {
             Console.WriteLine("First branch updated");
             string secondBranch = "Created secondBranch";
-            string test = "Created on 18.May.2019 on PC";
         }
     }
 }
