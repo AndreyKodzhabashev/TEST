9718e2464320d619aca6c562e42cef5eeda1da6e
AndreyKo <andrey.kodzhabashev@gmail.com>
2019-05-28 13:15:28 +0300

    Added after unistalling the extention GitHub for Visual Studio

--- HowToWorkWithGitHub/01Test_GitHubNewProjectFromPC/StartUp.cs
+++ HowToWorkWithGitHub/01Test_GitHubNewProjectFromPC/StartUp.cs
@@ -12,6 +12,8 @@ namespace _01Test_GitHubNewProjectFromPC
             string test = "Created on GitHub with editing the file";
             string test1 = "Created on 18.May.2019 on my PC";
             string test3 = "Added again on PC";
+
+            string test4 = "Added after unistalling the extention GitHub for Visual Studio";
         }
     }
 }
